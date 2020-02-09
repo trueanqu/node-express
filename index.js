47730e1ca9778e321405496ef88f2b37056d0a13
trueanqu <bogdan.oblapenko@gmail.com>
2020-02-09 20:24:05 +0200

    add morgan and /public static files serving

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = "localhost"
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev')); // dev - print additional information to the screen
+
+app.use(express.static(__dirname + '/public')); //serve files from ./public directory (. is index location stored in __directory)
 
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
