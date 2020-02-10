2a12d06266fa763b81d95d48c3000ca3929751a0
trueanqu <bogdan.oblapenko@gmail.com>
2020-02-10 21:57:32 +0200

    Add support of /dishes/:dishId routes

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -6,11 +6,7 @@ const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/') //dishes uri will be passed in the index.js file, so here we are assuming, that we are working with root of '/dishes'
-.all((req, res, next) => { //next - net function, that is applicable to the /dishes resource
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-})
+.all(setCommonResponseData)
 .get((req, res, next) => {
     res.end('Will send all the dishes to you!');
 })
@@ -25,5 +21,31 @@ dishRouter.route('/') //dishes uri will be passed in the index.js file, so here 
     res.statusCode = 403; //not supported status code
     res.end('PUT operation is not supported on /dishes');
 });
+
+dishRouter.route('/:dishId')
+.all(setCommonResponseData)
+.all((req, res, next) => {
+    res.setHeader('dishId',`${req.params.dishId}`);
+    next();
+})
+.get((req, res, next) => {
+    res.end('Will send details about dish with id:' + req.params.dishId + ' to you!');
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation is not supported on /dishes/' + req.params.dishId);
+})
+.delete((req, res, next) => {
+    res.end('Deleting the dish with id:' + req.params.dishId + ', name:"' + req.body.name + '", description:"' + req.body.description + '"');
+})
+.put((req, res, next) => {
+    res.end('Updating entry with id:' + req.params.dishId + ' ... Setting name:"' + req.body.name + '", description:"' + req.body.description + '"');
+});
+
+function setCommonResponseData(req, res, next) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+}
 
 module.exports = dishRouter;
